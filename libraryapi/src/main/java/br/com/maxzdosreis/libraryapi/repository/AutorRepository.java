5e10a233a149d3029b55ee809eb36f791b384f5b
Max Zimmermann dos Reis <maxzdosreis@gmail.com>
2025-07-24 00:22:03 -0300

    refactor: tipo de variável no AutorRepository errada

--- libraryapi/src/main/java/br/com/maxzdosreis/libraryapi/repository/AutorRepository.java
+++ libraryapi/src/main/java/br/com/maxzdosreis/libraryapi/repository/AutorRepository.java
@@ -3,6 +3,7 @@ package br.com.maxzdosreis.libraryapi.repository;
 import br.com.maxzdosreis.libraryapi.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,5 +13,5 @@ public interface AutorRepository extends JpaRepository<Autor, Long> {
     List<Autor> findByNacionalidade(String nacionalidade);
     List<Autor> findByNomeAndNacionalidade(String nome, String nacionalidade);
 
-    Optional<Autor> findByNomeAndDataNascimentoAndNacionalidade(String nome, String dataNascimento, String nacionalidade);
+    Optional<Autor> findByNomeAndDataNascimentoAndNacionalidade(String nome, LocalDate dataNascimento, String nacionalidade);
 }
