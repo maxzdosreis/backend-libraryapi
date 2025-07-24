a898a910e9aeaa8a9bf6f694bf9e5cad60231e0b
Max Zimmermann dos Reis <maxzdosreis@gmail.com>
2025-07-24 00:23:50 -0300

    feat: cria os validators de Autor e Livro

--- libraryapi/src/main/java/br/com/maxzdosreis/libraryapi/validator/AutorValidator.java
+++ libraryapi/src/main/java/br/com/maxzdosreis/libraryapi/validator/AutorValidator.java
@@ -1,4 +1,37 @@
 package br.com.maxzdosreis.libraryapi.validator;
 
+import br.com.maxzdosreis.libraryapi.dto.AutorComLivrosDTO;
+import br.com.maxzdosreis.libraryapi.dto.AutorDTO;
+import br.com.maxzdosreis.libraryapi.exception.DuplicateRegistration;
+import br.com.maxzdosreis.libraryapi.model.Autor;
+import br.com.maxzdosreis.libraryapi.repository.AutorRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
 public class AutorValidator {
+
+    public AutorRepository repository;
+
+    public AutorValidator(AutorRepository repository) {
+        this.repository = repository;
+    }
+
+    public void validarAutor(Autor autor) {
+        if (existeAutorCadastrado(autor)) {
+            throw new DuplicateRegistration(("Autor já cadastrado!"));
+        }
+    }
+
+    private boolean existeAutorCadastrado(Autor autor) {
+        Optional<Autor> autorEncontrado = repository.findByNomeAndDataNascimentoAndNacionalidade(
+                autor.getNome(), autor.getDataNascimento(), autor.getNacionalidade()
+        );
+
+        if(autor.getId() == null){
+            return autorEncontrado.isPresent();
+        }
+        return !autor.getId().equals(autorEncontrado.get().getId()) && autorEncontrado.isPresent();
+    }
 }
--- libraryapi/src/main/java/br/com/maxzdosreis/libraryapi/validator/LivroValidator.java
+++ libraryapi/src/main/java/br/com/maxzdosreis/libraryapi/validator/LivroValidator.java
@@ -1,4 +1,48 @@
 package br.com.maxzdosreis.libraryapi.validator;
 
+import br.com.maxzdosreis.libraryapi.exception.DuplicateRegistration;
+import br.com.maxzdosreis.libraryapi.exception.InvalidFieldException;
+import br.com.maxzdosreis.libraryapi.model.Livro;
+import br.com.maxzdosreis.libraryapi.repository.LivroRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
 public class LivroValidator {
+
+    private static final int ANO_EXIGENCIA_PRECO = 2018;
+
+    private final LivroRepository repository;
+
+    public LivroValidator(LivroRepository repository) {
+        this.repository = repository;
+    }
+
+    public void validar(Livro livro) {
+        if(existeLivroComIsbn(livro)){
+            throw new DuplicateRegistration("ISBN já registrado!");
+        }
+
+        if(isPrecoObrigatorioNulo(livro)){
+            throw new InvalidFieldException("preco", "Para livros com ano de publicação a partir de 2018, o preço é obrigatório");
+        }
+    }
+
+    private boolean isPrecoObrigatorioNulo(Livro livro){
+        return livro.getPreco() == null && livro.getDataPublicacao().getYear() >= ANO_EXIGENCIA_PRECO;
+    }
+
+    private boolean existeLivroComIsbn(Livro livro){
+        Optional<Livro> livroEncontrado = repository.findByIsbn(livro.getIsbn());
+
+        if (livro.getId() == null) {
+            return livroEncontrado.isPresent();
+        }
+
+        return livroEncontrado
+                .map(Livro::getId)
+                .stream()
+                .anyMatch(id -> !id.equals(livro.getId()));
+    }
 }
